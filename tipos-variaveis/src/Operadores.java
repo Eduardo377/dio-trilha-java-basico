fe7c60412d93efbfeb5259bc486c2dec550fd03b
Eduardo377 <eduardogomes377@gmail.com>
2023-03-24 01:37:58 -0300

    fix: tipos de variaveis e operadores

--- tipos-variaveis/src/Operadores.java
+++ tipos-variaveis/src/Operadores.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.text.NumberFormat;
 
-public class TiposVariaveis {
+public class Operadores {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         double a, b;
